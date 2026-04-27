fb63d24d2e390cec95836d3aed270e93b1c5f8a0
younesazimi21 <younesazimi21@gmail.com>
2026-04-27 13:15:14 +0200

    Arbejdet i MenuDAOimpl klassen metoderne korrekt dog justeres når menu klassen også justeres.

--- SEP2/Server/src/Repository/MenuDAOImpl.java
+++ SEP2/Server/src/Repository/MenuDAOImpl.java
@@ -2,10 +2,9 @@ package Repository;
 
 import model.Menu;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import javax.swing.plaf.nimbus.State;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MenuDAOImpl implements MenuDAO
@@ -29,38 +28,94 @@ public class MenuDAOImpl implements MenuDAO
   @Override
   public Menu create (int id, String name, String allergies, double price) throws SQLException
   {
-    try(Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres?currentSchema=jdbc", "postgres", "admin"))
+    try (Connection connection = DriverManager.getConnection(
+        "jdbc:postgresql://localhost:5432/postgres?currentSchema=jdbc",
+        "postgres", "admin"))
     {
-      PreparedStatement statement =
-          connection.prepareStatement("INSERT INTO Menu(id,name, allergies, price) VALUES (?,?,?,?);");
-      statement.setInt(1, id);
-      statement.setString(2, name);
-      statement.setString(3, allergies);
-      statement.setDouble(4, price);
+      PreparedStatement statement = connection.prepareStatement(
+          "INSERT INTO menu(name, allergies, price) VALUES (?,?,?) RETURNING id");
 
-      statement.executeUpdate();
-      return new Menu(id, name, allergies, price);
+      statement.setString(1, name);
+      statement.setString(2, allergies);
+      statement.setDouble(3, price);
+
+      ResultSet rs = statement.executeQuery();
+
+      if (rs.next())
+      {
+        int generatedId = rs.getInt("id");
+        return new Menu(generatedId, name, allergies, price);
+      }
     }
+    return null;
   }
 
   @Override public Menu readById(int id) throws SQLException
   {
-    return null;
+    try(Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres?currentSchema=jdbc", "postgres", "admin"))
+    {
+      PreparedStatement statement = connection.prepareStatement("SELECT * FROM Menu WHERE id = ?");
+      statement.setInt(1, id);
+      ResultSet resultSet = statement.executeQuery();
+      if (resultSet.next())
+      {
+        String name = resultSet.getString("name");
+        String allergies = resultSet.getString("allergies");
+        double price = resultSet.getDouble("price");
+
+        return new Menu(id, name, allergies, price);
+      }
+      else
+      {
+        return null;
+      }
+    }
   }
 
-  @Override public List<Menu> readByName(String searchString)
-      throws SQLException
+  @Override public List<Menu> readByName(String searchString) throws SQLException
   {
-    return List.of();
+    try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres?currentSchema=jdbc", "postgres", "admin"))
+    {
+      PreparedStatement statement = connection.prepareStatement("SELECT * FROM Menu WHERE name LIKE ?");
+      statement.setString(1, "%" + searchString + "%");
+      ResultSet resultSet = statement.executeQuery();
+      ArrayList<Menu> result = new ArrayList<>();
+      while (resultSet.next())
+      {
+        int id = resultSet.getInt("id");
+        String name = resultSet.getString("name");
+        String allergies = resultSet.getString("allergies");
+        double price = resultSet.getDouble("price");
+        Menu menu = new Menu(id, name, allergies, price);
+        result.add(menu);
+      }
+      return result;
+    }
   }
 
   @Override public void update(Menu menu) throws SQLException
   {
-
+    try(Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres?currentSchema=jdbc", "postgres", "admin"))
+    {
+      PreparedStatement statement =
+          connection.prepareStatement(
+              "UPDATE menu SET name = ?, allergies = ?, price = ? WHERE id = ?");
+      statement.setString(1, menu.getName());
+      statement.setString(2, menu.getAllergies());
+      statement.setDouble(3, menu.getPrice());
+      statement.setInt(4, menu.getId());
+      statement.executeUpdate();
+    }
   }
 
   @Override public void delete(Menu menu) throws SQLException
   {
-
+    try(Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres?currentSchema=jdbc", "postgres", "admin"))
+    {
+      PreparedStatement statement =
+          connection.prepareStatement("DELETE FROM menu WHERE id = ?");
+      statement.setInt(1, menu.getId());
+      statement.executeUpdate();
+    }
   }
 }
