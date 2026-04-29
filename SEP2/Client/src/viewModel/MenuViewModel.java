67144c6ca0906ede6f8a18c4f483e82c11a904b4
Lini Rose <75529247+LiniRose@users.noreply.github.com>
2026-04-29 14:57:07 +0200

    view models

--- SEP2/Client/src/viewModel/MenuViewModel.java
+++ SEP2/Client/src/viewModel/MenuViewModel.java
@@ -1,5 +1,35 @@
 package viewModel;
 
-public class MenuViewModel
+import javafx.beans.property.*;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import model.Model;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+public class MenuViewModel implements PropertyChangeListener
 {
+  private Model model;
+
+  public MenuViewModel(Model model)
+  {
+    this.model = model;
+
+  }
+
+  public void clear()
+  {
+    loadFromModel();
+  }
+
+  public void loadFromModel()
+  {
+
+  }
+
+  @Override public void propertyChange(PropertyChangeEvent evt)
+  {
+
+  }
 }
